f1dd6921eb095b7812e261743bbec974f13a4cc8
David Gallardo <dgall033@ucr.edu>
2023-03-13 10:16:31 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace testApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeting = "Hello";
+            var ending = "World";
+            Console.WriteLine($"{greeting} { ending}");
+        }
+    }
+}
